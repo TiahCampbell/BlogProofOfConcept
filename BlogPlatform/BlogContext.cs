d540e7b916d088c93c75559431d35b1186c8c0e4
unknown <cuevastl@gmail.com>
2021-03-05 15:00:16 -0500

    Added seed data to database and completed first migration.

--- BlogPlatform/BlogContext.cs
+++ BlogPlatform/BlogContext.cs
@@ -16,50 +16,75 @@ namespace blog_template_practice
         {
             var connectionString = "Server=(localdb)\\mssqllocaldb;Database=BlogDB_templatetest;Trusted_Connection=True;";
 
-            optionsBuilder.UseSqlServer(connectionString)
-                .UseLazyLoadingProxies();
+            optionsBuilder.UseSqlServer(connectionString).UseLazyLoadingProxies();
 
             base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Add seed data for Category model:
             modelBuilder.Entity<Category>().HasData(
                 new Category()
                 {
-
+                    Id = 1,
+                    Name = "Fitness"
                 },
                 new Category()
                 {
-
+                    Id = 2,
+                    Name = "Nutrition"
                 },
                 new Category()
                 {
-
+                    Id = 3,
+                    Name = "Pets"
                 },
                 new Category()
                 {
-
+                    Id = 4,
+                    Name = "Family"
                 }
                 );
 
             modelBuilder.Entity<Content>().HasData(
                 new Content()
                 {
-
+                    Id = 1,
+                    Title = "Strong",
+                    Body = "Working out by learning a new Strong class to teach! It's taking time to get my strength and stamina back after baby, but I can do full push ups again! Little wins...getting stronger everyday!",
+                    Author = "Teacup",
+                    PublishDate = "03/02/21",
+                    CategoryId = 1
                 },
                 new Content()
                 {
-
+                    Id = 2,
+                    Title = "Lentil Tacos",
+                    Body = "Made lentil tacos for dinner tonight. I grabbed a new recipe offline I wanted to try. The recipe used veggie broth and finished it off with mixing in salsa at the end. Definitely yummy, would eat again.",
+                    Author = "Teacup",
+                    PublishDate = "03/03/21",
+                    CategoryId = 2
                 },
                 new Content()
                 {
-
+                    Id = 3,
+                    Title = "Thief",
+                    Body = "So my dog is obsessed with our babies new toy. Keep in mind Washburne got three new toys only a week ago for his 6th birthday. This week I bought a new set of stacking teething rings for Miles and he has already stolen and destroyed two of the rings. Upon seeing how distraught my husband and I were when he destroyed the first two, he has decided that he should not chew them up. This has not stopped him from periodically stealing the smallest blue teething ring and hiding it around the house for himself.",
+                    Author = "Teacup",
+                    PublishDate = "03/05/21",
+                    CategoryId = 3
+                },
+                new Content()
+                {
+                    Id = 4,
+                    Title = "Crawling",
+                    Body = "Miles is on the move. We have been frantically baby proofing the house as our 6 month old baby has already learned to crawl and pull himself up on furniture. We spent a ridiculous amount of money on two retractable mesh baby gates on Amazon this week to rig up a way to protect the entertainment center and two xbox series x's from his little grabbing hands. Pardon our dust.",
+                    Author = "Teacup",
+                    PublishDate = "03/04/21",
+                    CategoryId = 4
                 }
                 );
 
-            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ BlogPlatform/Migrations/20210305195501_Initial.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogPlatform.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Body = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    PublishDate = table.Column<string>(nullable: true),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contents_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Fitness" },
+                    { 2, "Nutrition" },
+                    { 3, "Pets" },
+                    { 4, "Family" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contents",
+                columns: new[] { "Id", "Author", "Body", "CategoryId", "PublishDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Teacup", "Working out by learning a new Strong class to teach! It's taking time to get my strength and stamina back after baby, but I can do full push ups again! Little wins...getting stronger everyday!", 1, "03/02/21", "Strong" },
+                    { 2, "Teacup", "Made lentil tacos for dinner tonight. I grabbed a new recipe offline I wanted to try. The recipe used veggie broth and finished it off with mixing in salsa at the end. Definitely yummy, would eat again.", 2, "03/03/21", "Lentil Tacos" },
+                    { 3, "Teacup", "So my dog is obsessed with our babies new toy. Keep in mind Washburne got three new toys only a week ago for his 6th birthday. This week I bought a new set of stacking teething rings for Miles and he has already stolen and destroyed two of the rings. Upon seeing how distraught my husband and I were when he destroyed the first two, he has decided that he should not chew them up. This has not stopped him from periodically stealing the smallest blue teething ring and hiding it around the house for himself.", 3, "03/05/21", "Thief" },
+                    { 4, "Teacup", "Miles is on the move. We have been frantically baby proofing the house as our 6 month old baby has already learned to crawl and pull himself up on furniture. We spent a ridiculous amount of money on two retractable mesh baby gates on Amazon this week to rig up a way to protect the entertainment center and two xbox series x's from his little grabbing hands. Pardon our dust.", 4, "03/04/21", "Crawling" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_CategoryId",
+                table: "Contents",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contents");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
