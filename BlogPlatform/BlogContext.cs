dde92fd91479a465a3775194736eefd163fdc3a4
unknown <cuevastl@gmail.com>
2021-03-09 23:09:53 -0500

    Completed css, added migrations.

--- BlogPlatform/BlogContext.cs
+++ BlogPlatform/BlogContext.cs
@@ -58,20 +58,20 @@ namespace blog_template_practice
                     Body = "Working out by learning a new Strong class to teach! It's taking time to get my strength and stamina back after baby, but I can do full push ups again! Little wins...getting stronger everyday!",
                     Author = "Teacup",
                     Image = "Images/Strong.jpg",
-                    PublishDate = "03/02/21",
+                    PublishDate = "3/2/2021 05:35:21 PM",
                     CategoryId = 1,
                     ColorId = 1
                 },
                 new Content()
                 {
                     Id = 2,
-                    Title = "Lentil Tacos",
-                    Body = "Made lentil tacos for dinner tonight. I grabbed a new recipe offline I wanted to try. The recipe used veggie broth and finished it off with mixing in salsa at the end. Definitely yummy, would eat again.",
+                    Title = "Meal Prep",
+                    Body = "Made some quinoa with tofu, mushrooms, tomato, red bell peppers, and broccoli for meal prep this week. Topped it with some avacado for those healthy fats!",
                     Author = "Teacup",
-                    Image = "Images/Nutrition.jpg",
-                    PublishDate = "03/03/21",
+                    Image = "Images/MealPrep.jpg",
+                    PublishDate = "3/3/2021 04:22:32 PM",
                     CategoryId = 2,
-                    ColorId = 1
+                    ColorId = 2
                 },
                 new Content()
                 {
@@ -80,9 +80,9 @@ namespace blog_template_practice
                     Body = "So my dog is obsessed with our babies new toy. Keep in mind Washburne got three new toys only a week ago for his 6th birthday. This week I bought a new set of stacking teething rings for Miles and he has already stolen and destroyed two of the rings. Upon seeing how distraught my husband and I were when he destroyed the first two, he has decided that he should not chew them up. This has not stopped him from periodically stealing the smallest blue teething ring and hiding it around the house for himself.",
                     Author = "Teacup",
                     Image = "Images/Thief.jpg",
-                    PublishDate = "03/05/21",
+                    PublishDate = "3/5/2021 06:12:02 PM",
                     CategoryId = 3,
-                    ColorId = 1
+                    ColorId = 5
                 },
                 new Content()
                 {
@@ -91,7 +91,51 @@ namespace blog_template_practice
                     Body = "Miles is on the move. We have been frantically baby proofing the house as our 6 month old baby has already learned to crawl and pull himself up on furniture. We spent a ridiculous amount of money on two retractable mesh baby gates on Amazon this week to rig up a way to protect the entertainment center and two xbox series x's from his little grabbing hands. Pardon our dust.",
                     Author = "Teacup",
                     Image = "Images/Crawling.jpg",
-                    PublishDate = "03/04/21",
+                    PublishDate = "3/4/2021 08:42:55 PM",
+                    CategoryId = 4,
+                    ColorId = 4
+                },
+                new Content()
+                {
+                    Id = 5,
+                    Title = "Spartan",
+                    Author = "Teacup",
+                    Body = "It's been a few years since my husband and I ran the Spartan Sprint in 2018 with my students. It was a blast and an experience to remember. Climbing, running, mud pits, and coaching my students through that fear of heights! Even though we got pulled off the course for a freak lightning storm, we stuck it out and got back out there after the storm cleared. Jumping that fire was an awesome moment! Giving my body the time to heal and working to get back to where I was before baby. But my goal is to run another one in 2022. I can't wait!",
+                    Image = "Images/Spartan2.jpg",
+                    PublishDate = "3/6/2021 06:11:22 PM",
+                    CategoryId = 1,
+                    ColorId = 6
+                },
+                new Content()
+                {
+                    Id = 6,
+                    Title = "Mango",
+                    Body = "Miles is starting to eat solid foods! He enjoys blueberries, broccoli, and tofu so far. Today he tried mango and it was an instant hit!",
+                    Author = "Teacup",
+                    Image = "Images/Mango.jpg",
+                    PublishDate = "3/7/2021 11:02:43 PM",
+                    CategoryId = 2,
+                    ColorId = 3
+                },
+                new Content()
+                {
+                    Id = 7,
+                    Title = "Guitar",
+                    Body = "Washburne is six years old now. Over the years he has learned to accept Nate playing guitar. Don't let that fool you though, this drama puppy used to lay there and cry anytime Nate even opened his guitar case as a wee pup.",
+                    Author = "Teacup",
+                    Image = "Images/Guitar.jpg",
+                    PublishDate = "3/9/2021 10:38:41 PM",
+                    CategoryId = 3,
+                    ColorId = 7
+                },
+                new Content()
+                {
+                    Id = 9,
+                    Title = "Backyard",
+                    Body = "Miles went on his first adventure in the backyard today! He got to play with leaves, zoom down his little tykes slide, and toddle after his doggo around the yard! Excited for more days like this!",
+                    Author = "Teacup",
+                    Image = "Images/Backyard.jpg",
+                    PublishDate = "3/8/2021 10:21:48 PM",
                     CategoryId = 4,
                     ColorId = 1
                 }
